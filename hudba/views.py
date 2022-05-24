8262e95e549b89f0fe728b3ad4bb7c1d3ca1e63a
vacexpfleger <c4n4l123@gmail.com>
2022-05-24 10:19:27 +0200

    v1.2.4 - úpravy šablon

--- hudba/views.py
+++ hudba/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from hudba.models import Album, Track, Review
--- hudbadb/settings.py
+++ hudbadb/settings.py
@@ -121,3 +121,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
