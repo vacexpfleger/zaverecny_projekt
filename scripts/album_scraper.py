ae2582994bbd7ba73f62f52c420674d8780dc20c
vacexpfleger <c4n4l123@gmail.com>
2022-10-24 19:27:33 +0200

    v1.2.9 - úpravy scraperu

--- scripts/album_scraper.py
+++ scripts/album_scraper.py
@@ -5,27 +5,43 @@ import re
 import wikipedia
 
 
-def scrape(artist, album, url):
-    URL = url
-    page = requests.get(URL)
-
+def scrape(url):
+    page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
 
-    summary = wikipedia.summary(album)
+    album = soup.find("h1", id="firstHeading").text
+    print(album)
+    artist = soup.find("div", class_="contributor").text
+    print(artist)
     genres = soup.find("th", string="Genre").next_sibling
     length = soup.find("th", string="Length").next_sibling.text
     label = soup.find("th", string="Label").next_sibling.text
     released = soup.find("th", string="Released").next_sibling.text
-    released = re.sub('\s?\((.*?)\)', '', released)
+    released = re.sub(r'\s?\((.*?)\)', '', released)
+    summary = wikipedia.summary(f"{album} (album)")
     tracklist = soup.find_all("table", class_="tracklist")
     tracks_list = []
     genres_list = []
 
-    for genre in genres.find_all("li"):
-        genres_list.append(re.sub('\s?\[(.*?)]', '', genre.text))
+    if not genres.find_all("li"):
+        for genre in genres.find_all("a"):
+            genres_list.append(re.sub(r'\s?\[(.*?)]', '', genre.text))
+    else:
+        for genre in genres.find_all("li"):
+            genres_list.append(re.sub(r'\s?\[(.*?)]', '', genre.text))
 
-    for track in tracklist:
-        tracks_list.extend(re.findall(r'\".*?\"', track.text))
+    if len(tracklist) == 1:
+        for track in tracklist:
+            tracks_list.extend(re.findall(r'\".*?\"', track.text))
+    else:
+        print(f"There are {len(tracklist)} tracklists in total:")
+        for track in tracklist:
+            print(track.find("caption").text)
+        choice = list(map(int, input("Enter multiple values: ").split()))
+        for x in choice:
+            tracks_list.extend(re.findall(r'\".*?\"', tracklist[x-1].text))
+
+    genres_list = [i for i in genres_list if i]
 
     print(f"{album}, {artist}")
     print("Genres: ", genres_list)
@@ -37,10 +53,5 @@ def scrape(artist, album, url):
 
 
 def run():
-    artist = input("Artist name: ")
-    album = input("Album name: ")
     url = input("Album Wiki URL: ")
-    scrape(artist, album, url)
-
-
-
+    scrape(url)
